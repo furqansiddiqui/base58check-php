f8bcd4ea426179d4c1e51aac6edb9223acbb3189
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-22 13:56:44 +0500

    Base58 encoding

---
+++ src/Base58.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/base58check-php" package.
+ * https://github.com/furqansiddiqui/base58check-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/base58check-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Base58;
+
+use FurqanSiddiqui\Base58\Result\Base58Encoded;
+use FurqanSiddiqui\BcMath\BcBaseConvert;
+use FurqanSiddiqui\BcMath\BcNumber;
+
+/**
+ * Class Base58
+ * @package FurqanSiddiqui\Base58
+ */
+class Base58
+{
+    public const CHARSET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+    /**
+     * @param string|null $charset
+     * @return string
+     */
+    private static function Charset(?string $charset = null): string
+    {
+        $charset = $charset ?? self::CHARSET;
+        if (strlen($charset) !== 58) {
+            throw new \LengthException('Base58 charsets must have exactly 58 digits');
+        }
+
+        return $charset;
+    }
+
+    /**
+     * @param BcNumber $decs
+     * @param string|null $charset
+     * @return Base58Encoded
+     */
+    public static function Encode(BcNumber $decs, ?string $charset = null): Base58Encoded
+    {
+        $base58 = BcBaseConvert::fromBase10($decs, self::Charset($charset));
+        $base58Encoded = new Base58Encoded($base58);
+        $base58Encoded->readOnly(true); // Set to read-only
+        return $base58Encoded;
+    }
+
+    /**
+     * @param Base58Encoded $encoded
+     * @param string|null $charset
+     * @return BcNumber
+     */
+    public static function Decode(Base58Encoded $encoded, ?string $charset = null): BcNumber
+    {
+        return BcBaseConvert::toBase10($encoded->get(), self::Charset($charset));
+    }
+
+    /**
+     * @param string $encoded
+     * @param string|null $charset
+     * @return BcNumber
+     */
+    public static function DecodeFromString(string $encoded, ?string $charset = null): BcNumber
+    {
+        return self::Decode(new Base58Encoded($encoded), self::Charset($charset));
+    }
+}
---
+++ src/Result/Base58Encoded.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/base58check-php" package.
+ * https://github.com/furqansiddiqui/base58check-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/base58check-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Base58\Result;
+
+use FurqanSiddiqui\DataTypes\Buffer\AbstractStringType;
+
+/**
+ * Class Base58Encoded
+ * @package FurqanSiddiqui\Base58\Result
+ */
+class Base58Encoded extends AbstractStringType
+{
+    /**
+     * Base58Encoded constructor.
+     * @param string $encoded
+     * @param string|null $charset
+     */
+    public function __construct(string $encoded, ?string $charset = null)
+    {
+        if (!$encoded) {
+            throw new \InvalidArgumentException('Base58Encoded objects cannot be constructed without data');
+        }
+
+        if ($charset) {
+            if (!preg_match('/^[' . preg_quote($charset, '/') . ']+$/', $encoded)) {
+                throw new \InvalidArgumentException('Encoded string does not match given Base58 charset');
+            }
+        }
+
+        parent::__construct($encoded);
+    }
+}
